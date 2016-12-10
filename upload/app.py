dbd61ac0357af5c3cbfb7b44e8835d77f15744cd
sgalkina <galkina.s.a@gmail.com>
2016-12-10 14:31:18 +0100

    feat: auth support

--- upload/app.py
+++ upload/app.py
@@ -1,21 +1,30 @@
 import asyncio
 from aiohttp import web
 import aiohttp_cors
-from upload import iloop_client, logger
-from upload.settings import Default
 import pandas as pd
 from pandas.io.common import CParserError
 import io
 import requests
 import json
+from functools import wraps, partial
 from upload.upload import MediaUploader, StrainsUploader, ExperimentUploader
 from tempfile import mkstemp
-from upload import __version__
 from potion_client.exceptions import ItemNotFound
+from upload import iloop_client, __version__, logger
+from upload.settings import Default
 from upload.checks import compound_name_unknown, medium_name_unknown, strain_alias_unknown, \
     experiment_identifier_unknown, synonym_to_chebi_name
 
-iloop = iloop_client(Default.ILOOP_API, Default.ILOOP_TOKEN)
+
+def call_iloop_with_token(f):
+    @wraps(f)
+    async def wrapper(request):
+        token = Default.ILOOP_TOKEN
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].replace('Bearer ', '')
+        iloop = iloop_client(Default.ILOOP_API, token)
+        return await f(request, iloop)
+    return wrapper
 
 
 def write_temp_csv(data_string):
@@ -26,7 +35,8 @@ def write_temp_csv(data_string):
     return tmp_file_name
 
 
-async def upload(request):
+@call_iloop_with_token
+async def upload(request, iloop):
     data = await request.post()
     try:
         project = iloop.Project.first(where={'code': data['project_id']})
@@ -41,8 +51,8 @@ async def upload(request):
         if data['what'] == 'media':
             content = data['file[0]'].file.read().decode()
             uploader = MediaUploader(project, write_temp_csv(content),
-                                     custom_checks=[compound_name_unknown],
-                                     synonym_mapper=synonym_to_chebi_name)
+                                     custom_checks=[partial(compound_name_unknown, iloop)],
+                                     synonym_mapper=partial(synonym_to_chebi_name, iloop))
         if data['what'] == 'strains':
             content = data['file[0]'].file.read().decode()
             uploader = StrainsUploader(project, write_temp_csv(content))
@@ -51,11 +61,11 @@ async def upload(request):
             content_physiology = data['file[1]'].file.read().decode()
             uploader = ExperimentUploader(project, write_temp_csv(content_samples),
                                           write_temp_csv(content_physiology),
-                                          custom_checks=[compound_name_unknown,
-                                                         experiment_identifier_unknown,
-                                                         medium_name_unknown,
-                                                         strain_alias_unknown],
-                                          synonym_mapper=synonym_to_chebi_name)
+                                          custom_checks=[partial(compound_name_unknown, iloop),
+                                                         partial(experiment_identifier_unknown, iloop),
+                                                         partial(medium_name_unknown, iloop),
+                                                         partial(strain_alias_unknown, iloop)],
+                                          synonym_mapper=partial(synonym_to_chebi_name, iloop))
     except CParserError:
         return web.json_response(
             data={'valid': False, 'tables': [{'errors': [{'message': 'failed to parse csv file '}]}]})
@@ -64,7 +74,6 @@ async def upload(request):
     try:
         uploader.upload(iloop=iloop)
     except ItemNotFound as error:
-        print('fooo')
         return web.json_response(
             data={'valid': False, 'tables': [{'errors': [{'message': str(error)}]}]})
     else:
@@ -75,22 +84,25 @@ async def hello(request):
     return web.Response(text='hi, this is upload v' + __version__)
 
 
-app = web.Application()
-app.router.add_route('POST', '/upload', upload)
-app.router.add_route('GET', '/upload/hello', hello)
+ROUTE_CONFIG = [
+    ('POST', '/upload', upload),
+    ('GET', '/upload/hello', hello),
+]
 
+app = web.Application()
 # Configure default CORS settings.
 cors = aiohttp_cors.setup(app, defaults={
     "*": aiohttp_cors.ResourceOptions(
-        allow_credentials=True,
         expose_headers="*",
         allow_headers="*",
+        allow_credentials=True,
     )
 })
 
-# Configure CORS on all routes.
-for route in list(app.router.routes()):
-    cors.add(route)
+for method, path, handler in ROUTE_CONFIG:
+    resource = app.router.add_resource(path)
+    cors.add(resource)
+    cors.add(resource.add_route("GET", handler))
 
 
 async def start(loop):
--- upload/tests/test_inspection.py
+++ upload/tests/test_inspection.py
@@ -2,11 +2,16 @@
 Tests for the inspecting files
 
  """
-import upload.upload as cup
+import json
+import pytest
+from functools import partial
 from os.path import abspath, join, dirname
 import pandas as pd
-import pytest
-import json
+import upload.upload as cup
+from upload import iloop_client
+from upload.settings import Default
+from upload.checks import experiment_identifier_unknown, medium_name_unknown, \
+    strain_alias_unknown, compound_name_unknown, synonym_to_chebi_name
 
 
 @pytest.fixture(scope='session')
@@ -39,7 +44,6 @@ def test_strains_inspection(examples):
     with pytest.raises(ValueError) as excinfo:
         cup.StrainsUploader('TST', join(examples, 'strains-invalid.csv'))
     report = json.loads(str(excinfo.value))
-    print(report)
     assert report['error-count'] == 1
     error = report['tables'][0]['errors'].pop()
     assert 'bad expected gnomic' in error['message']
@@ -47,5 +51,22 @@ def test_strains_inspection(examples):
 
 def test_experiment_inspection(examples):
     up = cup.ExperimentUploader('TST', join(examples, 'samples.csv'), join(examples, 'physiology.csv'), [])
+    assert isinstance(up.samples_df, pd.DataFrame)
+    assert isinstance(up.physiology_df, pd.DataFrame)
+
+
+def test_experiment_inspection_with_iloop(examples):
+    iloop = iloop_client(Default.ILOOP_API, Default.ILOOP_TOKEN)
+    up = cup.ExperimentUploader('TST',
+                                join(examples, 'samples.csv'),
+                                join(examples, 'physiology.csv'),
+                                custom_checks=[
+                                    partial(compound_name_unknown, iloop),
+                                    partial(experiment_identifier_unknown,
+                                            iloop),
+                                    partial(medium_name_unknown, iloop),
+                                    partial(strain_alias_unknown, iloop)],
+                                synonym_mapper=partial(synonym_to_chebi_name,
+                                                       iloop))
     assert isinstance(up.samples_df, pd.DataFrame)
     assert isinstance(up.physiology_df, pd.DataFrame)
