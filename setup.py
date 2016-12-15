3ec9ca88399418218d5dc67082c3780c03eea290
Henning Redestig <henning.red@gmail.com>
2016-12-15 11:22:08 +0100

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='upload',
-    version='0.1.0',
+    version='0.2.0',
     description="uploading data to iloop for dd-decaf",
     long_description=readme,
     author="Henning Redestig",
