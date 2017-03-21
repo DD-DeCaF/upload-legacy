c1c345c6acd2130f7a46ee7ffc8e9e41eb6132f3
Henning Redestig <henning.red@gmail.com>
2017-03-21 15:55:01 +0100

    Bump version: 0.2.11 → 0.2.12

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='upload',
-    version='0.2.11',
+    version='0.2.12',
     description="uploading data to iloop for dd-decaf",
     long_description=readme,
     author="Henning Redestig",
--- upload/__init__.py
+++ upload/__init__.py
@@ -26,4 +26,4 @@ def _isnan(value):
 
 
 __author__ = 'Henning Redestig'
-__version__ = '0.2.11'
+__version__ = '0.2.12'
