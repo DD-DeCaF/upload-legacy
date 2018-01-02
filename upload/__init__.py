1ae9135f57f7f9fc0fb0f68ff612c97857a8f98e
Ali Kaafarani <ali@kvikshaug.no>
2018-01-02 20:20:01 +0100

    refactor: move log configuration to __init__ module

--- upload/__init__.py
+++ upload/__init__.py
@@ -4,10 +4,22 @@ from potion_client import Client
 from potion_client.auth import HTTPBearerAuth
 import requests
 import numpy as np
+from raven import Client as RavenClient
+from raven.conf import setup_logging
+from raven.handlers.logging import SentryHandler
+
+from . import settings
+
 
 logger = logging.getLogger('upload')
 logger.addHandler(logging.StreamHandler(stream=sys.stdout))  # Logspout captures logs from stdout if docker containers
 logger.setLevel(logging.DEBUG)
+
+# Configure Raven to capture warning logs
+raven_client = RavenClient(settings.Default.SENTRY_DSN)
+handler = SentryHandler(raven_client)
+handler.setLevel(logging.WARNING)
+setup_logging(handler)
 
 
 def iloop_client(api, token):
--- upload/middleware.py
+++ upload/middleware.py
@@ -1,18 +1,4 @@
-import logging
-
-from aiohttp import web
-from raven import Client
-from raven.conf import setup_logging
-from raven.handlers.logging import SentryHandler
-
-from . import settings
-
-
-# Configure Raven to capture warning logs
-client = Client(settings.Default.SENTRY_DSN)
-handler = SentryHandler(client)
-handler.setLevel(logging.WARNING)
-setup_logging(handler)
+from . import raven_client
 
 
 async def raven_middleware(app, handler):
@@ -21,6 +7,6 @@ async def raven_middleware(app, handler):
         try:
             return await handler(request)
         except Exception:
-            client.captureException()
+            raven_client.captureException()
             raise
     return middleware_handler
