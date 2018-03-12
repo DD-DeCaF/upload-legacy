d1b4473fef3b15072f5354b8ca476956dfaf73ac
Ali Kaafarani <ali@kvikshaug.no>
2018-03-12 17:24:12 +0100

    style: newline after license

--- upload/constants.py
+++ upload/constants.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 synonym_to_chebi_name_dict = {
     'o2': 'dioxygen',
     'co2': 'carbon dioxide',
